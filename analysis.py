a632d24f2b9e0f444438ed9df62af0299fcf98f0
Grainne10 <grainne_boyle@outlook.com>
2024-04-06 14:23:35 +0100

    Download Iris DataSet

---
+++ analysis.py
@@ -0,0 +1,10 @@
+# analysis.py
+# This project is for the programme and scripting module in the Higher Diploma in Science in Data Analytics
+# It is based on the Fisher's Iris Data Set
+# Author Grainne Boyle
+
+# The first step is to import Pandas, a python library used for working with datasets
+import pandas as pd
+
+# Next I load the Iris data set into a data frame using df in pandas
+df = pd.read_csv("C:\Users\grain\Desktop\cmder\pands\pands-project\iris.data")
