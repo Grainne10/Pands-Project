280380a59e555af79a9df637636dc5fbb27d3427
Grainne10 <grainne_boyle@outlook.com>
2024-04-09 19:13:33 +0100

    Import CSV file and test data

--- analysis.py
+++ analysis.py
@@ -3,25 +3,27 @@
 # It is based on the Fisher's Iris Data Set
 # Author Grainne Boyle
 
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-iris = fetch_ucirepo(id=53) 
-  
-# data (as pandas dataframes) 
-X = iris.data.features 
-y = iris.data.targets 
-  
-# metadata 
-print(iris.metadata) 
-  
-# variable information 
-print(iris.variables) 
-
-# The first step is to import Pandas, a python library used for working with datasets
-
-
+# The first step is to import pandas . Pandas is an open source library used for data analysis and manipulation of data frames/datasets. 
 
 import pandas as pd
 
 # Next I load the Iris data set into a data frame using df in pandas
+
+df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+#print(df.info())
+#print(df.describe())
+print(df.isnull())
+#print(df.head())
+#print (df)
+#print(df.dtypes)
+#print(df.describe)
+# To get the petal lengths
+#plen = df['petal_length']
+# Show
+#print(plen)
+# type
+#print(type(plen))
+#just to get the numpy array
+#plen.to_numpy()
+
+
