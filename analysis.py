bdf1057aa37ac169cf15a6ec24a886c42d6a0c18
Grainne10 <grainne_boyle@outlook.com>
2024-05-14 21:35:26 +0100

    Add descriptions to  plots and libraries

--- analysis.py
+++ analysis.py
@@ -6,12 +6,12 @@
 # The first step is to import pandas . Pandas is an open source library used for data analysis and manipulation of data frames/datasets. 
 
 import pandas as pd
-
+ 
 # Next I load the Iris data set into a data frame using df in pandas
 # A DataFrame is a 2 dimensional data structure, like a 2 dimensional array,or a table with rows and columns.
 df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
 
-#I do some review of the data, you can remove some of the hashtags to view these
+#I do some review of the data
 # It gives you a brief overview of the data by columns, count, types
 print(df.info())
 # Brief description of the count, mean, std dev, min
@@ -31,11 +31,13 @@ print(plen)
 # type
 print(type(plen))
 
-# to get the numpy array
+# to get the numpy array. 
 plen.to_numpy()
 
-# If we want to see the number of each Species column, we can use this:
+# If we want to see the number of each Species column, we can use this formula.
+# We can see we have 3 unique species. All species contains equal amounts or rows.
 df["species"].value_counts() 
+
 
 # Output a summary of the each variables to a single text file.
 
@@ -67,32 +69,30 @@ with open(r"iris_summary.txt", "w") as f:
 # The data set is clean, has no missing values and is ready for analysis.
 
 
-
 # We will look at how some of these individual variables look different charts and plots.
-# In order to do this, we must import matplotlib, it is a library for data visualization.
+# In order to do this, we must import matplotlib, it is a library for creating visualisations in python. It is generally used for basic chart plotting.
+
+ 
 import matplotlib.pyplot as plt
 
-import matplotlib.pyplot as plt
 
-
-# We will install Numpy
+# We will install Numpy. NumPy is a python library, it can be used to perform advanced mathematical tasks quickly.
 
 import numpy as np
 
 
-
-
-
-# Seaborn can be imported and used by matplotlib to draw its plots
+# Seaborn can be imported and used by matplotlib to draw its plots.  
+# Seaborn is another python data visualisation library based on matplotlib, it integrates with pandas data structures. 
+# It provides aesthetic data visualisations.
 import seaborn as sns
-# We can pull in the colour palette from seaborn and set our colours to distinguish the different iris species.
 
 #To ignore warnings, re: the figure layout changes, we import the warnings module
 import warnings
 warnings.filterwarnings('ignore')
 
-
-# Draw a scatterplot of the sepal length vs sepal width and show the plots in different shapes to identify them clearly
+# Scatter plots use dots to represent values for two different numeric variables.
+# Scatter plots are used to observe relationships between variables.
+# This is a scatterplot of the sepal length vs sepal width and I show the plots in different shapes to identify them clearly
 sns.scatterplot(df, x="sepal_length", y="sepal_width", hue="species", style="species", markers={"setosa": "^", "versicolor": "s", "virginica": "o"})
 plt.title("Scatter Plot of Sepal Length vs. Sepal Width")
 plt.xlabel("Sepal Length (Cm) ")
@@ -101,15 +101,17 @@ plt.legend(loc='upper right', bbox_to_anchor=(1.30, 1))
 plt.savefig('Scatter plot of Sepal Length vs Sepal Width ', bbox_inches='tight')
 plt.show()# Show a boxplot of the four variables
 
+# This is a scatterplot of the petal length vs petal width and I show the plots in different shapes to identify them clearly.
 sns.scatterplot(df, x="petal_length", y="petal_width", hue="species", style="species", markers={"setosa": "^", "versicolor": "s", "virginica": "o"})
-plt.title("Scatter Plot of Peta Length vs. Petal Width")
+plt.title("Scatter Plot of Petal Length vs. Petal Width")
 plt.xlabel("Petal Length (Cm) ")
 plt.ylabel("Petal Width (Cm) ")
 plt.legend(loc='upper right', bbox_to_anchor=(1.30, 1)) 
 plt.savefig('Scatter plot of Petal Length vs Petal Width ', bbox_inches='tight')
 plt.show()
 
-# Show the variable in a pairplot using seaborn. This shows the scatter plots and kernal density plots for all variables
+# A pairplot combines both histogram and scatter plots, it provides an overview of each pair of variables and shows the dataset's distributions and correlations. If the variables tend to increase and decrease together, the association is positive.
+# This is a pairplot using seaborn. This shows the scatter plots and kernal density plots for all variables
 sns.pairplot(df, hue="species", markers=["o", "s", "^"])
 plt.suptitle("Pairplot of Iris Dataset", y=1.02)
 plt.savefig('Pairplot of Iris Dataset', bbox_inches='tight')
@@ -117,31 +119,35 @@ plt.show()
 
 
 
-# From the scatterplot, 
-# We can show the scatterplots separately using facet grids:
+# Facet grids maps the dataset onto multiple axes.
+# It visualises the distribution of variables of the dataset and the relationship between variables. 
+# I use it to show each of the species separately and clearly. T
+# This facet grid shows a scatterplot of sepal length vs sepal width
 
 g = sns.FacetGrid(df, col="species",  hue="species", height=5, aspect=1.2)
 g.map(sns.scatterplot, "sepal_length", "sepal_width")
 g.set_axis_labels("Sepal Length (cm)", "Sepal Width (cm)")
 g.set_titles("{col_name}")
-
 plt.subplots_adjust(top=0.85) 
 plt.suptitle("Facet Grid Sepal Length  vs. Sepal Width ", fontsize=18)
 plt.savefig('Facet Grid Scatter plot of Sepal Length vs Sepal Width of Iris Species', bbox_inches='tight')
 plt.show()
 
+# This facet grid shows a scatterplot of petal length vs petal width
 g = sns.FacetGrid(df, col="species",  hue="species", height=5, aspect=1.2)
 g.map(sns.scatterplot, "petal_length", "petal_width")
 g.set_axis_labels("Petal Length (cm)", "Petal Width (cm)")
 g.set_titles("{col_name}")
-
 plt.subplots_adjust(top=0.85) 
 plt.suptitle("Facet Grid Petal Length  vs. Petal Width ", fontsize=18)
 plt.savefig('Facet Grid Scatter plot of Petal Length vs Petal Width of Iris Species', bbox_inches='tight')
 plt.show()
 
 
-
+# A boxplot demonstrates graphically the locality, spread and skewness of groups of numerical data through their quartiles.
+# The spacings in the subsections of the box-plot indicate the degree of spread and skewness of the data.
+# It also displays outliers that differ from the rest of the dataset.
+# I show a boxplot of the four variables in the dataset.
 plt.figure(figsize=(10, 8))
 sns.boxplot(data=df, orient="v", palette="Set2")
 plt.ylabel('Measurement (cm)')
@@ -149,7 +155,10 @@ plt.title('Box Plot of Iris Dataset')
 plt.savefig('Box Plot of Iris Dataset')
 plt.show()
 
-# Do a histogram of sepal length
+
+# Histograms are visual representation of the distribution of quantitative data.It takes all the data and divides the entire range of values into a series of intervals.
+# it counts how many values fall into each interval. 
+# This is a histogram of sepal length
 plt.figure(figsize=(6, 6))
 sns.histplot(df, x='sepal_length', hue="species", edgecolor = "white", multiple="layer", kde=True, bins=18, alpha=0.3)
 plt.title('Histogram Sepal Length')
@@ -160,7 +169,7 @@ plt.xticks(rotation=45)
 plt.savefig('Histogram Sepal Length')
 plt.show()
 
-# Do a histogram of sepal width
+# This is a histogram of sepal width
 plt.figure(figsize=(6, 6))
 sns.histplot(df, x='sepal_width', hue="species", edgecolor = "white", multiple="layer", kde=True, bins=16, alpha=0.3)
 plt.title('Histogram Sepal Width')
@@ -170,7 +179,7 @@ plt.ylabel('Count')
 plt.savefig('Histogram Sepal Width')
 plt.show()
 
-# Do a histogram of petal length
+# This is a histogram of petal length
 plt.figure(figsize=(6, 6))
 sns.histplot(df, x='petal_length', hue="species", edgecolor = "white", multiple="layer", kde=True, bins=18, alpha=0.3)
 plt.title('Histogram Petal Length')
@@ -180,7 +189,7 @@ plt.ylabel('Count')
 plt.savefig('Histogram Petal Length')
 plt.show()
 
-# Do a histogram of petal width
+# Thi is a histogram of petal width
 plt.figure(figsize=(6, 6))
 sns.histplot(df, x='petal_length', hue="species", edgecolor = "white", multiple="layer", kde=True, bins=18, alpha=0.3)
 plt.title('Histogram Petal Width')
@@ -201,15 +210,18 @@ print("Correlation Matrix:")
 print(correlation_matrix)
 
 # We can use the correlation matrix to plot a heatmap
+# Heatmaps use a colour coding system to represent different values. They allow you to quickly identify patterns and anomalies.
 # We can use annot to show the values and fmt for decimal places
+# This shows a heatmap of all the variables
 plt.figure(figsize=(8, 6))
 sns.heatmap(correlation_matrix, annot=True, cmap='YlGnBu', fmt=".2f", linewidths=0.5) 
 plt.title('Correlation Heatmap of Iris Dataset Variables')
 plt.savefig('Iris DataSet HeatMap')
 plt.show()
 
-
-# Prepare a violin plot for each variable in the Iris Data Set
+# A violin plot is a hybrid of a box plot and a kernel density plot, which shows peaks in the data. 
+#It is used to visualize the distribution of numerical data. 
+# This is a violin plot for each variable in the Iris Data Set
  
 fig, axes = plt.subplots(2, 2, figsize=(18, 10))
  
@@ -223,8 +235,8 @@ sns.violinplot(df, x='species', y='petal_width', ax=axes[1, 1])
 plt.savefig('Iris DataSet Violin Plots')
 plt.show()
 
-
-# Andrew's curves is an example of multivariate analysis:
+# Andrews Curves can be used as a way to visualise multivariate data. It maps all features from a row of data onto a function resulting in a unique curve. It highlights potential clusters or separations from them.
+# This is an Andrew's curves plot :
 
 plt.figure(figsize=(10, 8))
 pd.plotting.andrews_curves(df, 'species', colormap='plasma')
@@ -237,10 +249,4 @@ plt.show()
 #data = df.iloc[:, :-1]  # Selecting all rows and all columns except the last one
 #feature_names = df.columns[:-1]  # Extracting feature names from DataFrame columns
 
-#plt.figure(figsize=(8, 6))
-#plt.boxplot(data, labels=feature_names)
-#plt.xlabel('Features')
-#plt.ylabel('Feature Values')
-#plt.title('Box Plot of Iris Dataset Features')
-#plt.grid(True)
-#plt.show()
+
